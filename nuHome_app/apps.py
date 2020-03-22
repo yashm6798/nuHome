a84c32a0da50369553b6fd89a44b0ecc553d7111
Yash Dipak Mehta <yashdipm@andrew.cmu.edu>
2020-03-22 14:01:29 -0400

    Updated models.py for database classes

---
+++ nuHome_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NuhomeAppConfig(AppConfig):
+    name = 'nuHome_app'
---
+++ nuHome_app/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+REGION_CHOICES=()
+class Refugee_Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	avatar = models.FileField(blank=True)
+	bio = models.TextField(blank=True)
+	region = models.CharField(max_length=2, choices=REGION_CHOICES)
+	verification_status = models.BooleanField(default=False)
+
+class NGO_Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	avatar = models.FileField(blank=True)
+	bio = models.TextField(blank=True)
+	region = models.CharField(max_length=2, choices=REGION_CHOICES)
+
+class Post(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.TextField(blank=False)
+	content = models.TextField(blank=False)
+	STATUS_CHOICES = [('V', 'Verified'), ('U', 'Unverified'), ('F', 'False')]
+	status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+	CATEGORY_CHOICES = [('I', 'Important'), ('S', 'Social'), ('J', 'Jobs'), ('A', 'Accommodation'), ('R', 'Resources'), ('O', 'Other')]
+	category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+	date_time = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, on_delete = models.CASCADE)
+	date_time = models.DateTimeField(auto_now_add=True)
+	content = models.TextField(blank=False)
+
+class Message(models.Model):
+	from_user = models.ForeignKey(User, on_delete=models.CASCADE)
+	to_user = models.ForeignKey(User, on_delete=models.CASCADE)
+	date_time = models.DateTimeField(auto_now_add=True)
+	content = models.TextField(blank=False)
